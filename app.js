341e2292edcf61bc7a515816bb0058873882fd85
Valery Avdeev <vkavdeev@gmail.com>
2022-04-11 23:38:22 +0300

    feat: project build database connection

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const { PORT = 3000 } = process.env;
+
+mongoose.connect("mongodb://localhost:27017/mestodb", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
