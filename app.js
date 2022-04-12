5efeb388dd5a6085f3edf458d77cdd198214e36e
Valery Avdeev <vkavdeev@gmail.com>
2022-04-12 23:06:47 +0300

    feat: creating routes database request, error

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
 
 const { PORT = 3000 } = process.env;
 
-mongoose.connect("mongodb://localhost:27017/mestodb", {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.use('/', require('./routes/users'));
+
+app.listen(PORT, () => {
+  console.log(`App listening on port: ${PORT}`);
 });
---
+++ routes/users.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+
+const User = require('../models/user');
+
+router.get('/users', (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+});
+
+router.get('/users/:userId', (req, res) => {
+  User.find({})
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+});
+
+router.post('/users', (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+});
+
+module.exports = router;
