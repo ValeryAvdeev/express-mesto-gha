67ba08908c50849a86bae52057e8fad86328102f
Valery Avdeev <vkavdeev@gmail.com>
2022-04-16 12:37:23 +0300

    fix: finish practical 13

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,4 +1,6 @@
 const Card = require('../models/card');
+const { NotFoundError } = require('../error/NotFoundError');
+const { ValidationError } = require('../error/ValidationError');
 
 module.exports.getCards = (req, res) => {
   Card.find({})
@@ -6,28 +8,66 @@ module.exports.getCards = (req, res) => {
     .catch(() => res.status(500).send({ message: 'Произошла ошибка при получении карточек' }));
 };
 
-module.exports.postCard = (req, res) => {
+module.exports.postCard = (req, res, next) => {
   const { name, link } = req.body;
 
   Card.post({ name, link })
     .then((card) => res.send({ card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new ValidationError('Переданы некорректные данные при создании карточки'));
+      }
+      next('Произошла ошибка');
+    });
 };
 
-module.exports.deleteCard = (req, res) => {
+module.exports.deleteCard = (req, res, next) => {
   Card.findByIdAndRemove(req.params.id)
     .then((card) => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'NotFoundError') {
+        next(new NotFoundError('Карточка с указанным _id не найдена.'));
+      }
+      next('Произошла ошибка');
+    });
 };
 
-module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
-  req.params.cardId,
-  { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
-  { new: true },
-);
+module.exports.likeCard = (req, res, next) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { new: true },
+  )
+    .then((card) => {
+      if (!card) {
+        throw new NotFoundError('Передан несуществующий _id карточки.');
+      }
+      res.status(200).send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'Validation') {
+        next(new ValidationError('Карточка с указанным _id не найдена.'));
+      }
+      next('Произошла ошибка');
+    });
+};
 
-module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
-  req.params.cardId,
-  { $pull: { likes: req.user._id } }, // убрать _id из массива
-  { new: true },
-);
+module.exports.dislikeCard = (req, res, next) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { new: true },
+  )
+    .then((card) => {
+      if (!card) {
+        throw new NotFoundError('Передан несуществующий _id карточки.');
+      }
+      res.status(200).send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'Validation') {
+        next(new ValidationError('Карточка с указанным _id не найдена.'));
+      }
+      next('Произошла ошибка');
+    });
+};
--- controllers/users.js
+++ controllers/users.js
@@ -2,14 +2,13 @@ const User = require('../models/user');
 const { NotFoundError } = require('../error/NotFoundError');
 const { ValidationError } = require('../error/ValidationError');
 
-// работает корректо
+// работает
 module.exports.getUsers = (req, res, next) => {
   User.find({})
     .then((users) => res.send({ data: users }))
     .catch(next);
 };
 
-// работает корректо
 module.exports.getUserId = async (req, res, next) => {
   try {
     const userId = await User.findById(req.params.id);
@@ -25,7 +24,8 @@ module.exports.getUserId = async (req, res, next) => {
   }
 };
 
-// работает корректо
+// работает
+
 module.exports.postUser = (req, res, next) => {
   const { name, about, avatar } = req.body;
 
@@ -44,7 +44,6 @@ module.exports.createMe = (req, res, next) => {
 
   User.findByIdAndUpdate(req.params.id, { name, about }, { new: true, runValidators: true })
     .then((user) => {
-      console.log(user);
       if (!user) {
         next(new NotFoundError('Пользователь с указанным _id не найден'));
       }
@@ -56,19 +55,26 @@ module.exports.createMe = (req, res, next) => {
       } else if (err.name === 'NotFoundError') {
         next(new NotFoundError('Пользователь с указанным _id не найден'));
       }
-      res.status(500).send({ message: 'Произошла ошибка' });
+      next(err);
     });
 };
 
-module.exports.createMeAvatar = (req, res) => {
-  User.findByIdAndUpdate(req.params.id, { avatar }, { new: true })
-    .then((avatar) => res.send({ data: avatar }))
+module.exports.createMeAvatar = (req, res, next) => {
+  const { avatar } = req.body;
+
+  User.findByIdAndUpdate(req.params.id, { avatar }, { new: true, runValidators: true })
+    .then((userAvatar) => {
+      if (!userAvatar) {
+        next(new NotFoundError('Пользователь с указанным _id не найден'));
+      }
+      res.send({ data: userAvatar });
+    })
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля.' });
+        next(new ValidationError('Переданы некорректные данные при обновлении профиля.'));
       } else if (err.name === 'NotFoundError') {
-        res.status(404).send({ message: 'Пользователь с указанным _id не найден' });
+        next(new NotFoundError('Пользователь с указанным _id не найден'));
       }
-      res.status(500).send({ message: 'Произошла ошибка' });
+      next(err);
     });
 };
