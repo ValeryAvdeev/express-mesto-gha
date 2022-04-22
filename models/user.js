086b42a46cbaed3a37489d119270abde28939735
Valery Avdeev <vkavdeev@gmail.com>
2022-04-22 20:51:06 +0300

    feat: create login add email, password

--- models/user.js
+++ models/user.js
@@ -1,21 +1,36 @@
 const mongoose = require('mongoose');
+const isEmail = require('validator/es/lib/isEmail');
 
 const userSchema = new mongoose.Schema({
-  name: {
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: (v) => isEmail(v),
+      message: 'Неправильный формат почты',
+    },
+  },
+  password: {
     type: String,
     required: true,
+    minlength: 8,
+  },
+  name: {
+    type: String,
+    default: 'Жак-Ив Кусто',
     minlength: 2,
     maxlength: 30,
   },
   about: {
     type: String,
-    required: true,
+    default: 'Исследователь',
     minlength: 2,
     maxlength: 30,
   },
   avatar: {
     type: String,
-    required: true,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
 });
 
