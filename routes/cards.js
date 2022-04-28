f3822237677301452f9eed5c22e081b973e1b88b
Valery Avdeev <vkavdeev@gmail.com>
2022-04-28 19:17:50 +0300

    feat: fix bag cards del like dislike

--- routes/cards.js
+++ routes/cards.js
@@ -20,19 +20,19 @@ router.post('/', celebrate({
 
 router.delete('/:cardId', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.objectId(),
+    cardId: Joi.string().length(24).hex(),
   }),
 }), deleteCard);
 
 router.put('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.objectId(),
+    cardId: Joi.string().length(24).hex(),
   }),
 }), likeCard);
 
 router.delete('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.objectId(),
+    cardId: Joi.string().length(24).hex(),
   }),
 }), dislikeCard);
 
