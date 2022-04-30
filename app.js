6887b6bb6b3509bed00b33b092f7f0e8ce19fa45
Valery Avdeev <vkavdeev@gmail.com>
2022-04-30 18:57:44 +0300

    fix: review notes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const { celebrate, Joi, errors } = require('celebrate');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const { NotFoundError } = require('./error/NotFoundError');
 const errorHandler = require('./middlewares/error');
 
 const { PORT = 3000 } = process.env;
@@ -28,7 +29,7 @@ app.post('/signin', celebrate({
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
+    password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().pattern(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/),
@@ -39,9 +40,7 @@ app.use(auth);
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.use((req, res) => {
-  res.status(404).send({ message: 'страница не найдена' });
-});
+app.use((req, res, next) => next(new NotFoundError('страница не найдена')));
 
 app.use(errors());
 
--- models/user.js
+++ models/user.js
@@ -11,7 +11,6 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-    minlength: 8,
     select: false,
   },
   name: {
@@ -29,6 +28,7 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: [validator.isURL, 'введите URL адрес'],
   },
 });
 
